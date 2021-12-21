98cbb39fa8615e4cbf641c06ba183b32225f69c0
este6 <estebanchavrier@gmail.com>
2021-12-21 17:23:16 +0100

    insta

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,11 +1,19 @@
 <template>
 
   <div class="section contact">
-    <h1>Contact</h1>
-    <div class="SUIVEZ-NOUS">
+     
+     <h1>Contact</h1>
+  <div class="SUIVEZ-NOUS">
     <h1>SUIVEZ NOUS</h1>
+    <img src="pictures/insta.webp">
     <P>@skatechope</P>
     </div>
+
+
+<script async
+    src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
+</script>
+   </img>
   </div>
 
   
