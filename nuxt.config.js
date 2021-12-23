0a26285ae5dee812e24a87e243d444a806d65318
Oskar <oskar.codes@gmail.com>
2021-12-23 18:57:10 +0100

    add Skate Chope title tag to HEAD

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'skate-shope',
+    title: 'skate-chope',
     htmlAttrs: {
       lang: 'en'
     },
